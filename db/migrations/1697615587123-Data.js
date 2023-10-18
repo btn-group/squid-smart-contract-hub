3092d3313c77d82660167b5e692f56bccb81fc25
Steven Chang <stevenchang5000@protonmail.com>
2023-10-18 18:00:15 +1000

    Group has many group users

--- db/migrations/1697615587123-Data.js
+++ db/migrations/1697615587123-Data.js
@@ -1,11 +1,11 @@
-module.exports = class Data1696719908788 {
-    name = 'Data1696719908788'
+module.exports = class Data1697615587123 {
+    name = 'Data1697615587123'
 
     async up(db) {
-        await db.query(`CREATE TABLE "group" ("id" character varying NOT NULL, "name" text NOT NULL, "enabled" boolean NOT NULL, CONSTRAINT "PK_256aa0fda9b1de1a73ee0b7106b" PRIMARY KEY ("id"))`)
         await db.query(`CREATE TABLE "group_user" ("id" character varying NOT NULL, "account_id" text NOT NULL, "role" text NOT NULL, "group_id" character varying, CONSTRAINT "PK_c637f43a6f0d7891fec59f4d7a7" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_d339f18d53e39b898da78bbabb" ON "group_user" ("group_id") `)
         await db.query(`CREATE INDEX "IDX_813edd80a219ffee7b38103c31" ON "group_user" ("account_id") `)
+        await db.query(`CREATE TABLE "group" ("id" character varying NOT NULL, "name" text NOT NULL, "enabled" boolean NOT NULL, CONSTRAINT "PK_256aa0fda9b1de1a73ee0b7106b" PRIMARY KEY ("id"))`)
         await db.query(`CREATE TABLE "smart_contract" ("id" character varying NOT NULL, "address" text NOT NULL, "chain" integer NOT NULL, "caller" text NOT NULL, "enabled" boolean NOT NULL, "azero_id" text NOT NULL, "abi_url" text NOT NULL, "contract_url" text, "wasm_url" text, "audit_url" text, "project_name" text, "project_website" text, "github" text, "group_id" character varying, CONSTRAINT "PK_27627aca2eebd2eb72f26f6399a" PRIMARY KEY ("id"))`)
         await db.query(`CREATE INDEX "IDX_1f2f899cc57aa4ab9dc042c68f" ON "smart_contract" ("address") `)
         await db.query(`CREATE INDEX "IDX_23e08edcdb253cd2dfc3b51ad2" ON "smart_contract" ("caller") `)
@@ -16,10 +16,10 @@ module.exports = class Data1696719908788 {
     }
 
     async down(db) {
-        await db.query(`DROP TABLE "group"`)
         await db.query(`DROP TABLE "group_user"`)
         await db.query(`DROP INDEX "public"."IDX_d339f18d53e39b898da78bbabb"`)
         await db.query(`DROP INDEX "public"."IDX_813edd80a219ffee7b38103c31"`)
+        await db.query(`DROP TABLE "group"`)
         await db.query(`DROP TABLE "smart_contract"`)
         await db.query(`DROP INDEX "public"."IDX_1f2f899cc57aa4ab9dc042c68f"`)
         await db.query(`DROP INDEX "public"."IDX_23e08edcdb253cd2dfc3b51ad2"`)
